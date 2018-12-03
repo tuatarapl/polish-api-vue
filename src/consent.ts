2d5dbe4e260ac18fc228bed139fb224be0d341dc
Grzegorz Abramczyk <grzegorz.abramczyk@tuatara.pl>
2018-12-03 15:46:34 +0100

    transfer data control

--- src/consent.ts
+++ src/consent.ts
@@ -428,6 +428,116 @@ Vue.component('sender-pis-domestic-edit', {
     }
 })
 
+Vue.component('frequency-edit', {
+    props: ['value', 'readonly'],
+    template: `
+<div class="ml-4">
+    <template v-if="value">
+        <div class="form-group">
+            <label for="periodType">Period Type</label>
+            <select type="text" class="form-control" id="periodType"
+                v-model="value.periodType" :disabled="readonly">
+                <option>day</option>
+                <option>week</option>
+                <option>month</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="periodValue">Period Value</label>
+            <input type="text" class="form-control" id="periodValue"
+                v-model.number="value.periodValue" :readonly="readonly"/>
+        </div>
+    </template>
+    <button type="button" class="btn btn-primary" @click="doInitialize()" v-if="!readonly && !value">
+        Initialize
+    </button>
+</div>
+`,
+    methods: {
+        doInitialize() {
+            this.$emit('input', {})
+        }
+    }
+})
+
+Vue.component('recurrence-edit', {
+    props: ['value', 'readonly'],
+    template: `
+<div class="ml-4">
+    <template v-if="value">
+        <div class="form-group">
+            <label for="startDate">Start Date</label>
+            <input type="date" class="form-control" id="startDate"
+                v-model="value.startDate" :readonly="readonly"/>
+        </div>
+        <h4>Frequency</h4>
+        <frequency-edit v-model="value.frequency"></frequency-edit>
+        <div class="form-group">
+            <label for="endDate">End Date</label>
+            <input type="date" class="form-control" id="endDate"
+                v-model="value.endDate" :readonly="readonly"/>
+        </div>
+        <div class="form-group">
+            <label for="dayOffOffsetType">Day Offset Type</label>
+            <select type="text" class="form-control" id="dayOffOffsetType"
+                v-model="value.dayOffOffsetType" :disabled="readonly">
+                <option>before</option>
+                <option>after</option>
+            </select>
+        </div>
+    </template>
+    <button type="button" class="btn btn-primary" @click="doInitialize()" v-if="!readonly && !value">
+        Initialize
+    </button>
+</div>
+`,
+    methods: {
+        doInitialize() {
+            this.$emit('input', {})
+        }
+    }
+})
+
+Vue.component('transfer-data-edit', {
+    props: ['value', 'readonly'],
+    template: `
+<div class="ml-4">
+    <template v-if="value">
+        <div class="form-group">
+            <label for="description">Description</label>
+            <input type="text" class="form-control" id="description"
+                v-model="value.description" :readonly="readonly"/>
+        </div>
+        <div class="form-group">
+            <label for="amount">Amount</label>
+            <input type="text" class="form-control" id="amount"
+                v-model="value.amount" :readonly="readonly"/>
+        </div>
+        <div class="form-group">
+            <label for="executionDate">Execution Date</label>
+            <input type="date" class="form-control" id="executionDate"
+                v-model="value.executionDate" :readonly="readonly"/>
+        </div>
+        <h3>Recurrence</h3>
+        <recurrence-edit v-model="value.recurrence"></recurrence-edit>
+        <div class="form-group">
+            <label for="currency">Currency</label>
+            <input type="text" class="form-control" id="currency"
+                v-model="value.currency" :readonly="readonly"/>
+        </div>
+    </template>
+    <button type="button" class="btn btn-primary" @click="doInitialize()" v-if="!readonly && !value">
+        Initialize
+    </button>
+</div>
+`,
+    methods: {
+        doInitialize() {
+            this.$emit('input', {})
+        }
+    }
+})
+
 Vue.component('privilege-domestic-transfer-edit', {
     props: ['privilege', 'readonly'],
     template: `
@@ -442,6 +552,8 @@ Vue.component('privilege-domestic-transfer-edit', {
     <recipient-pis-edit v-model="privilege.recipient" :readonly="readonly"></recipient-pis-edit>
     <h2>Sender</h2>
     <sender-pis-domestic-edit v-model="privilege.sender" :readonly="readonly"></sender-pis-domestic-edit>
+    <h2>Transfer Data</h2>
+    <transfer-data-edit v-model="privilege.transferData" :readonly="readonly"></transfer-data-edit>
 </div>
     `,
     data() {
