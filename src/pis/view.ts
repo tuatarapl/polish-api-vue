c0abf4e74070f5f133d88b436ed279e8af1622a6
Grzegorz Abramczyk <grzegorz.abramczyk@tuatara.pl>
2018-12-04 10:46:22 +0100

    Recipient foreign view

--- src/pis/view.ts
+++ src/pis/view.ts
@@ -30,3 +30,28 @@ Vue.component('recipient-pis-view', {
 </section>
 `
 })
+
+Vue.component('recipient-pis-foreign-view', {
+    props: ['recipient'],
+    template: `
+<section class="ml-4" v-if="recipient">
+    <header>Recipient</header>
+    <dl class="row" v-if="recipient.accountNumber">
+        <dt class="col-4">Account Number</dt>
+        <dd class="col-8"> {{recipient.accountNumber}}</dd>
+    </dl>
+    <dl class="row" v-if="recipient.name">
+        <dt class="col-4">Name</dt>
+        <dd class="col-8"> {{recipient.name}}</dd>
+    </dl>
+    <dl class="row" v-if="recipient.address">
+        <dt class="col-4">Address</dt>
+        <dd class="col-8">
+            <ul class="list-unstyled">
+                <li v-for="item in recipient.address">{{item}}</li>
+            </ul>
+        </dd>
+    </dl>
+</section>
+`
+})
